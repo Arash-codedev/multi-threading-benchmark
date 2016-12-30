147b0ddc5f79232738d14ef17abf25695cd9c97a
Arash <Arash-codedev@users.noreply.github.com>
2016-12-30 13:29:07 +1100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,9 @@
 #include <vector>
 #include <thread>
 #include "libs/profiler.hpp"
-#include "libs/filesystem.hpp"
 
-const long range_from=10000000l*10l;
-const long range_to=20000000l*10l;
+const long range_from=10000000l/10l;
+const long range_to=20000000l/10l;
 
 bool is_prime(long a)
 {
@@ -20,15 +19,48 @@ bool is_prime(long a)
 	return true;
 }
 
-uint twin_range(long l1,long l2,int processDiv)
+struct Result
 {
-	uint count=0;
-	for(long l=l1;l<=l2;l+=long(processDiv))
-		if(is_prime(l) && is_prime(l+2))
+	double exe_time;
+	uint found;
+	ulong checks;
+	Result():
+		exe_time(0.0),
+		found(0),
+		checks(0)
+	{
+		// nothing 
+	}
+};
+
+void twin_range(long n_start,long n_stop,int index,int processDiv,Result *presult)
+{
+	// l1+(0,1,...,1000)*1
+	// l1+(0,1,...,1000)*2
+	// l1+(0,1,...,1000)*3
+	// ...
+
+	presult->checks=0;
+	presult->found=0;
+	const long chunks=1000;
+	long r_begin=0,k=0;
+	for(long i=0;r_begin<=n_stop;i++)
+	{
+		r_begin=n_start+(i*processDiv+index)*chunks;
+		for(k=r_begin;(k<r_begin+chunks) && (k<=n_stop);k++)
 		{
-			count++;
+			if(is_prime(k) && is_prime(k+2))
+			{
+				presult->found++;
+			}
+			presult->checks++;
 		}
-	return count;
+	}
+	std::cout
+		<<"Thread "<<index<<" finished task with "
+		<<(presult->checks)<<" checks and "
+		<<(presult->found)<<" results found"
+		<<std::endl<<std::flush;
 }
 
 
@@ -38,19 +70,25 @@ double main_multi_thread_test(int processDiv)
 	timer.tic();
 
 	std::vector<std::thread> thread_pool;
-	for(long i=0;i<processDiv;i++)
-		thread_pool.push_back(std::thread(twin_range,range_from+i,range_to,processDiv));
+	std::vector<Result> results;
+	results.assign(processDiv,Result());
+	for(int th_index=0;th_index<processDiv;th_index++)
+		thread_pool.push_back(std::thread(twin_range,range_from,range_to,th_index,processDiv,&results[th_index]));
 	// wait for tasks to finish
 	for(std::thread& th : thread_pool)
 		th.join();
+	ulong total_checks=0;
+	ulong total_found=0;
+	for(Result r:results)
+	{
+		total_checks+=r.checks;
+		total_found+=r.found;
+	}
+	std::cout
+		<<total_checks<<" numbers were checked. "
+		<<total_found<<" twin pairs detected."
+		<<std::endl;
 	return timer.toc();
-}
-
-double cpu_temperature()
-{
-	std::string temperature_file="/sys/class/hwmon/hwmon0/temp1_input";
-	std::string tmpr=filesystem::read_file(temperature_file);
-	return double(atol(tmpr.c_str()))/1000.0;
 }
 
 int main()
@@ -59,20 +97,18 @@ int main()
 	{
 		int N_thread;
 		double process_time;
-		double cpu_tempr;
 	};
 	std::vector<Info> info;
-	for (int N_thread=3;N_thread<40;N_thread++)
+	for (int N_thread=1;N_thread<=40;N_thread++)
 	{
 		double t=main_multi_thread_test(N_thread);
-		double cpu_t=cpu_temperature();
-		std::cout<<"Process time for N_thread= "<<N_thread<<" is "<<t<<" seconds. CPU temperature is "<<cpu_t<<std::endl<<std::flush;
-		info.push_back({N_thread,t,cpu_t});
+		std::cout<<"Process time for N_thread= "<<N_thread<<" is "<<t<<" seconds."<<std::endl;
+		info.push_back({N_thread,t});
 	}
 
 	std::cout<<"================================="<<std::endl;
 	for(Info v:info)
-		std::cout<<v.N_thread<<"\t"<<v.process_time<<"\t"<<v.cpu_tempr<<std::endl;
+		std::cout<<v.N_thread<<"\t"<<v.process_time<<"\t"<<std::endl;
 	std::cout<<"================================="<<std::endl;
 
 	return 0;
